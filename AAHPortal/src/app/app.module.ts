35ab58f8de8264b46071be13a0dc5a82492789fa
calebkg <calebkariuki05@gmail.com>
2025-07-07 21:22:45 +0300

    Fix AAHPortal Navigation and Styling

---
+++ AAHPortal/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { App } from './app';
+import { routes } from './app.routes';
+
+import { ProfileFormComponent } from './profile-form/profile-form.component';
+import { VacanciesComponent } from './vacancies/vacancies.component';
+import { MyApplicationsComponent } from './my-applications/my-applications.component';
+import { ApplicationFormComponent } from './application-form/application-form.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    ProfileFormComponent,
+    VacanciesComponent,
+    MyApplicationsComponent,
+    ApplicationFormComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  providers: [],
+  bootstrap: [App]
+})
+export class AppModule { }
--- AAHPortal/src/app/app.ts
+++ AAHPortal/src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,14 @@ import { CommonModule } from '@angular/common';
 })
 export class App {
   title = 'AAH Portal';
+  
+  constructor(private router: Router) {
+    // Track navigation events to ensure proper page loading
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Scroll to top on navigation
+      window.scrollTo(0, 0);
+    });
+  }
 }
