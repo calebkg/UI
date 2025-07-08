bea6e8804fd86b09d80b1680da0fe9dce7236d39
calebkg <calebkariuki05@gmail.com>
2025-07-08 11:12:22 +0300

    profile-summary page creation

--- AAHPortal/src/app/app.routes.ts
+++ AAHPortal/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { VacanciesComponent } from './vacancies/vacancies.component';
 import { MyApplicationsComponent } from './my-applications/my-applications.component';
 import { ApplicationFormComponent } from './application-form/application-form.component';
+import { ProfileSummaryComponent } from './profile-summary/profile-summary';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/profile', pathMatch: 'full' },
@@ -10,6 +11,6 @@ export const routes: Routes = [
   { path: 'vacancies', component: VacanciesComponent },
   { path: 'applications', component: MyApplicationsComponent },
   { path: 'apply/:id', component: ApplicationFormComponent },
-  { path: 'profile-summary', component: ProfileFormComponent },
+  { path: 'profile-summary', component: ProfileSummaryComponent },
   { path: 'edit-profile', component: ProfileFormComponent },
 ];
--- AAHPortal/src/app/profile-summary/profile-summary.ts
+++ AAHPortal/src/app/profile-summary/profile-summary.ts
@@ -1,11 +1,168 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+interface PersonalInfo {
+  firstName: string;
+  middleName: string;
+  email: string;
+  dateOfBirth: string;
+  gender: string;
+  nationality: string;
+}
+
+interface ContactInfo {
+  phoneNumber: string;
+  alternativePhone: string;
+  postalCode: string;
+  postalAddress: string;
+  physicalLocation: string;
+  street: string;
+}
+
+interface AcademicHistory {
+  institution: string;
+  qualification: string;
+  course: string;
+  specialisation: string;
+  award: string;
+  awardDate: string;
+}
+
+interface WorkExperience {
+  employer: string;
+  jobDesignation: string;
+  dateFrom: string;
+  dateTo: string;
+}
+
+interface Referee {
+  initials: string;
+  fullName: string;
+  mobileNumber: string;
+  email: string;
+  occupation: string;
+}
 
 @Component({
   selector: 'app-profile-summary',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FooterComponent],
   templateUrl: './profile-summary.html',
-  styleUrl: './profile-summary.scss'
+  styleUrls: ['./profile-summary.scss']
 })
-export class ProfileSummary {
+export class ProfileSummaryComponent implements OnInit {
 
+  personalInfo: PersonalInfo = {
+    firstName: 'Brianna',
+    middleName: 'Malina',
+    email: 'Kirui',
+    dateOfBirth: '20/06/1999',
+    gender: 'Male',
+    nationality: 'Kenyan'
+  };
+
+  contactInfo: ContactInfo = {
+    phoneNumber: '0704237858',
+    alternativePhone: '0785628357',
+    postalCode: '00100',
+    postalAddress: 'Nairobi',
+    physicalLocation: 'Westlands',
+    street: 'Woodvale'
+  };
+
+  academicHistory: AcademicHistory[] = [
+    {
+      institution: 'UON',
+      qualification: 'Degree',
+      course: 'Information Technology',
+      specialisation: 'Software Engineering',
+      award: 'Second Class',
+      awardDate: '11/07/23'
+    },
+    {
+      institution: 'TUK',
+      qualification: 'Diploma',
+      course: 'Information Technology',
+      specialisation: 'Software Engineering',
+      award: 'First Class',
+      awardDate: '11/07/23'
+    },
+    {
+      institution: 'UON',
+      qualification: 'Degree',
+      course: 'Information Technology',
+      specialisation: 'Software Engineering',
+      award: 'Second Class',
+      awardDate: '11/07/23'
+    }
+  ];
+
+  workExperience: WorkExperience[] = [
+    {
+      employer: 'Systra',
+      jobDesignation: 'software developer',
+      dateFrom: '11/07/23',
+      dateTo: '11/07/23'
+    },
+    {
+      employer: 'Systra',
+      jobDesignation: 'Software engineer intern',
+      dateFrom: '11/07/23',
+      dateTo: '11/07/23'
+    },
+    {
+      employer: 'Systra',
+      jobDesignation: 'software developer',
+      dateFrom: '11/07/23',
+      dateTo: '11/07/23'
+    },
+    {
+      employer: 'Systra',
+      jobDesignation: 'Software engineer intern',
+      dateFrom: '11/07/23',
+      dateTo: '11/07/23'
+    }
+  ];
+
+  referees: Referee[] = [
+    {
+      initials: 'Mr.',
+      fullName: 'Davis Decco',
+      mobileNumber: '0731856507',
+      email: 'decco54@gmail.com',
+      occupation: 'software developer'
+    },
+    {
+      initials: 'Mr.',
+      fullName: 'Davis Decco',
+      mobileNumber: '0731856507',
+      email: 'decco54@gmail.com',
+      occupation: 'software developer'
+    },
+    {
+      initials: 'Mr.',
+      fullName: 'Davis Decco',
+      mobileNumber: '0731856507',
+      email: 'decco54@gmail.com',
+      occupation: 'software developer'
+    },
+    {
+      initials: 'Mr.',
+      fullName: 'Davis Decco',
+      mobileNumber: '0731856507',
+      email: 'decco54@gmail.com',
+      occupation: 'software developer'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialize component or load data from service
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
